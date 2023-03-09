8617107902581399a2066d6edbb92bcb873328db
Marcelo Nascimento <marcelojunior@usp.br>
2023-03-09 12:55:43 -0300

    clean

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import dbPersons from './dbPersons'
-
-dbPersons.getAll().then(data => {
-  ReactDOM.createRoot(document.getElementById('root')).render(<App data={
-    data.map((obj) => ({...obj, number: [obj.number]}))
-  } />)
-})
